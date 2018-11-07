e867a781f0d0d2743240c2327b9686bb83768315
Petr Dihel <petr.dihel@shopsys.com>
2018-11-07 21:50:48 +0100

    Master

---
+++ feature/src/main/java/com/example/petr/asteroids/feature/Interfaces/GameObject.java
@@ -0,0 +1,10 @@
+package com.example.petr.asteroids.feature.Interfaces;
+
+import android.graphics.Canvas;
+
+public interface GameObject {
+
+    public void DrawOnCanvas(Canvas canvas);
+
+    public void GetBounds();
+}
